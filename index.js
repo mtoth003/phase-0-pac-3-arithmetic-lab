73f596c5ee3a7d4604b6b50d4258ea4e0de4101f
Michael Toth <mtoth003@gmail.com>
2021-09-02 00:58:07 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let x = 5;
+function increment(x) {
+    return ++x;
+}
+function decrement(x) {
+    return --x;
+}
+let n = "2.22";
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
+let a = "4x67"
+function makeInt(a) {
+    return parseInt(a, 10);
+}
